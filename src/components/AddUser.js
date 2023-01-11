515e945fdd8624ac36d162220bdd5dbb1134399e
RonaldoWidiantoSiswoyo <ronaldowidianto123@gmail.com>
2023-01-11 22:24:22 +0700

    first commit

---
+++ src/components/AddUser.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const AddUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("Male");
+  const navigate = useNavigate();
+
+  const saveUser = async (e) => {
+    e.preventDefault(); //ini berfungsi supaya pas di submit, page nya ga nge
+    try {
+      await axios.post("http://localhost:5000/users", {
+        name,
+        email,
+        gender,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="container ">
+      <div className="columns mt-1">
+        <div className="column is-half">
+          <form onSubmit={saveUser}>
+            <div className="field">
+              <label className="label">Name</label>
+              <div className="control">
+                <input
+                  type="text"
+                  className="input"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="name"
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  type="text"
+                  className="input"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Gender</label>
+              <div className="control">
+                <div className="select is-fullwitdth">
+                  <select
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <button type="submit" className="button is-success">
+                  Save
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AddUser;
---
+++ src/components/EditUser.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+
+const EditUser = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("Male");
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  useEffect(() => {
+    getUserById();
+  }, []);
+
+  const getUserById = async () => {
+    const response = await axios.get(`http://localhost:5000/users/${id}`);
+    setName(response.data.name);
+    setEmail(response.data.email);
+    setGender(response.data.gender);
+  };
+
+  const updateUser = async (e) => {
+    e.preventDefault(); //ini berfungsi supaya pas di submit, page nya ga nge
+    try {
+      await axios.patch(`http://localhost:5000/users/${id}`, {
+        name,
+        email,
+        gender,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="container ">
+      <div className="columns mt-1">
+        <div className="column is-half">
+          <form onSubmit={updateUser}>
+            <div className="field">
+              <label className="label">Name</label>
+              <div className="control">
+                <input
+                  type="text"
+                  className="input"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Name"
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  type="text"
+                  className="input"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  placeholder="Email"
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Gender</label>
+              <div className="control">
+                <div className="select is-fullwitdth">
+                  <select
+                    value={gender}
+                    onChange={(e) => setGender(e.target.value)}
+                  >
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+            <div className="field">
+              <div className="control">
+                <button type="submit" className="button is-success">
+                  Update
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditUser;
